7c572a7f022fb40d4f65a600ecac9bbc2549b064
Grecia Delgado <greciadm2596@gmail.com>
2021-03-24 11:32:38 -0500

    Refcator component TagItem and TagItemFill

--- src/components/UI/Tag.js
+++ src/components/UI/Tag.js
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { colors } from "../../ui";
 
 export function TagItem({ children }) {
   return <StyledDiv>{children}</StyledDiv>;
@@ -10,25 +11,25 @@ export function TagItemFill({ children }) {
 const StyledDivFill = styled.div`
   width: max-content;
   height: 45px;
-  background: #dfeaff;
+  background: ${colors.sky_blue};
   border-radius: 10px;
   padding: 12px 18px;
   display: flex;
   justify-content: center;
   p {
-    color: #2b4170;
+    color: ${colors.blue};
   }
 `;
 const StyledDiv = styled.div`
   width: max-content;
   background: #ffffff;
-  border: 1px solid #ffa85f;
+  border: 1px solid ${colors.orange};
   border-radius: 10px;
   padding: 5px 8px;
   display: flex;
   justify-content: center;
 
   p {
-    color: #ffa85f;
+    color: ${colors.orange};
   }
 `;
