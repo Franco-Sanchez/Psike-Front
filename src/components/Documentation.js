02f7efe38f1dae26bcd19179b5dc8f080c690d9e
carlos <carlostuna015@gmail.com>
2021-03-24 12:18:13 -0500

    componenet avatar created

--- src/components/Documentation.js
+++ src/components/Documentation.js
@@ -21,6 +21,7 @@ import {
   Heading5,
   Heading6,
 } from "./text/Heading";
+import Avatar from "./UI/Avatar";
 import FormField from "./UI/FormField";
 
 export default function Documentation() {
@@ -57,6 +58,9 @@ export default function Documentation() {
         <ContentXSB>Contraseña</ContentXSB>
         <input type="password" placeholder="****" />
       </FormField>
+
+      <Avatar url="https://cnnespanol.cnn.com/wp-content/uploads/2019/12/s_64a163f16ecbb099e52f2f8271f73cbbfcfc9034be4d646f7375e4db1ca6f3d7_1573501883482_ap_19001106049831-1.jpg?quality=100&strip=info&w=320&h=240&crop=1"/>
+      <Avatar url="https://i.pinimg.com/originals/67/54/78/675478c7dcc17f90ffa729387685615a.jpg"/>
     </div>
   );
 }
---
+++ src/components/UI/Avatar.js
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled"
+
+export default function Avatar({url}){
+    return(
+      <StyledAvatar url = {url} />
+    )
+}
+
+const StyledAvatar = styled.div`
+width:60px;
+height:60px;
+border-radius:50%;
+border:none;
+background-image:url(${(props)=>props.url});
+background-size:cover;
+`
