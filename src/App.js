ae4d44163bfe146ce97e187ac90b5b514141d64b
Grecia Delgado <greciadm2596@gmail.com>
2021-03-25 12:56:51 -0500

    Google API calendar conextion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { css, Global } from "@emotion/react";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Documentation from "./components/Documentation";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
@@ -11,6 +16,75 @@ import ShowPsychologist from "./pages/ShowPsychologist";
 import SignUp from "./pages/SignUp";
 
 function App() {
+  let gapi = window.gapi;
+  let CLIENT_ID =
+    "55420826679-ta7gh9fm2j3596fhk8setcqpkcka6d8g.apps.googleusercontent.com";
+  let API_KEY = "AIzaSyCKfwRJJ8JnkUUcxG5Lz2bwdvoM819yn5s";
+  let DISCOVERY_DOCS = [
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ];
+  let SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+  // client_id = 55420826679-ta7gh9fm2j3596fhk8setcqpkcka6d8g.apps.googleusercontent.com
+  // cliente secret = XoSdFreNpY9k558dtvQjBmcN
+  // api key = AIzaSyCKfwRJJ8JnkUUcxG5Lz2bwdvoM819yn5s
+
+  const handleClick = () => {
+    gapi.load("client:auth2", () => {
+      console.log("loaded client");
+
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      });
+
+      gapi.client.load("calendar", "v3", () => console.log("bam!"));
+
+      gapi.auth2
+        .getAuthInstance()
+        .signIn()
+        .then(() => {
+          let event = {
+            summary: "Google I/O 2015",
+            location: "800 Howard St., San Francisco, CA 94103",
+            description:
+              "A chance to hear more about Google's developer products.",
+            start: {
+              dateTime: "2015-05-28T09:00:00-07:00",
+              timeZone: "America/Los_Angeles",
+            },
+            end: {
+              dateTime: "2015-05-28T17:00:00-07:00",
+              timeZone: "America/Los_Angeles",
+            },
+            recurrence: ["RRULE:FREQ=DAILY;COUNT=2"],
+            attendees: [
+              { email: "lpage@example.com" },
+              { email: "sbrin@example.com" },
+            ],
+            reminders: {
+              useDefault: false,
+              overrides: [
+                { method: "email", minutes: 24 * 60 },
+                { method: "popup", minutes: 10 },
+              ],
+            },
+          };
+
+          let request = gapi.client.calendar.events.insert({
+            calendarId: "primary",
+            resource: event,
+          });
+
+          request.execute(function (event) {
+            window.open(event.htmlLink);
+            // appendPre("Event created: " + event.htmlLink);
+          });
+        });
+    });
+  };
+
   return (
     <div>
       <Global
@@ -22,7 +96,7 @@ function App() {
           }
         `}
       />
-
+      <button onClick={handleClick}>Separar cita</button>
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
