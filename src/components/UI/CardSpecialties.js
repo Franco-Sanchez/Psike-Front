b5171d5bddf67c3c52dfd1017a1b9a4fc11533a1
Grecia Delgado <greciadm2596@gmail.com>
2021-03-25 18:08:22 -0500

    Crad especialties done

--- src/components/UI/CardSpecialties.js
+++ src/components/UI/CardSpecialties.js
@@ -4,26 +4,74 @@ import { ContentL, ContentSB, ContentXSB } from "../text/Content";
 import { TagItemFill } from "./Tag";
 
 export default function CardSpecialties() {
+  const specialties = [
+    {
+      id: 1,
+      name: "Adicciones",
+      subespecialties: [
+        "Adicción a internet",
+        "Ludopatía",
+        "videojuegos y nuevas tecnologías",
+      ],
+    },
+    {
+      id: 2,
+      name: "Autoestima",
+      subespecialties: ["Autoestima baja e inseguridad"],
+    },
+    {
+      id: 3,
+      name: "Trastornos alimenticios",
+      subespecialties: ["Anorexia", "Bulimia", "Obesidad"],
+    },
+    {
+      id: 4,
+      name: "Relaciones Familiares",
+      subespecialties: ["Educación Hijos"],
+    },
+    { id: 5, name: "Duelo/Luto", subespecialties: ["Duelos y pérdidas"] },
+    { id: 6, name: "Agustia", subespecialties: ["Angustia"] },
+    { id: 7, name: "Ansiedad", subespecialties: ["Ansiedad"] },
+    { id: 8, name: "Depresión", subespecialties: ["Depresión"] },
+    {
+      id: 9,
+      name: "Control de Ira",
+      subespecialties: [
+        "Conductas agresivas",
+        "Ira",
+        "Irritabilidad e Histeria",
+      ],
+    },
+    {
+      id: 15,
+      name: "Sexualidad e identidad",
+      subespecialties: [
+        "Orientación - Identidad sexual",
+        " Infertilidad",
+        "Esterilidad",
+        "Acoso laboral",
+      ],
+    },
+  ];
+
+  // const keys = Object.keys(specialties);
+  // const numOfpages = specialties
+
   return (
     <CardContainer type="specialties">
       <StyledCard>
         <ContentL>Especialidades</ContentL>
-        <div className="especialtiesComents">
-          <TagItemFill>Alcholismo</TagItemFill>
-          <TagItemFill>Alcholismo</TagItemFill>
-          <TagItemFill>Alcholismo</TagItemFill>
-        </div>
 
-        <ContentSB>Adiciones</ContentSB>
-        <TagItemFill>Alcholismo</TagItemFill>
-        <TagItemFill>Alcholismo</TagItemFill>
-        <TagItemFill>Alcholismo</TagItemFill>
-        <TagItemFill>Alcholismo</TagItemFill>
-
-        <ContentSB>Atoestima</ContentSB>
-        <TagItemFill>Alcholismo</TagItemFill>
-        <TagItemFill>Alcholismo</TagItemFill>
-        <TagItemFill>Alcholismo</TagItemFill>
+        {specialties.map((key) => (
+          <>
+            <ContentXSB>{key.name}</ContentXSB>
+            <div className="especialtiesComents">
+              {key.subespecialties.map((item) => (
+                <TagItemFill>{item}</TagItemFill>
+              ))}
+            </div>
+          </>
+        ))}
       </StyledCard>
     </CardContainer>
   );
@@ -31,4 +79,9 @@ export default function CardSpecialties() {
 
 const StyledCard = styled.div`
   width: 100%;
+  & .especialtiesComents {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 4px;
+  }
 `;
