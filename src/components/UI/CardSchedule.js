5bcecab872362e4c247816676a4d0bf1a348f500
Franco <francorsr98@gmail.com>
2021-03-25 20:58:15 -0600

    Styles added to the selected day

--- src/components/UI/CardSchedule.js
+++ src/components/UI/CardSchedule.js
@@ -6,11 +6,12 @@ import { StyledCard } from '../showPsychologist/calendarStyles';
 
 export default function CardSchedule() {
   const [day, setDay] = useState(new Date())
-  console.log(day)
+
   return (
     <CardContainer type="schedule">
       <StyledCard>
         <SimpleReactComponent
+          selected={day}
           onSelect={setDay}
           headerPrevArrow={<Icon type={'arrowLeft'} size={25} />}
           headerNextArrow={<Icon type={'arrow'} size={25}/>}
--- src/components/showPsychologist/calendarStyles.js
+++ src/components/showPsychologist/calendarStyles.js
@@ -35,60 +35,70 @@ export const StyledCard = styled.div`
       cursor: pointer;
       font-family: 'Inter'
     }
-  }
 
-  & .calendar-month_header {
-    ${flex}
-    justify-content: space-between;
+    & .calendar-month_header {
+      ${flex}
+      justify-content: space-between;
 
-    & .is-prev, .is-next {
-      padding: 2px;
-      ${flex};
-      justify-content: center;
-      border-radius: 10px;
-      box-shadow: 1px 1px 13px -6px #000000; 
+      & .is-prev, .is-next {
+        padding: 2px;
+        ${flex};
+        justify-content: center;
+        border-radius: 10px;
+        box-shadow: 1px 1px 13px -6px #000000; 
+      }
+
+      & div {
+        font-weight: bold;
+      }
     }
 
-    & div {
-      font-weight: bold;
-    }
-  }
-
-  & .calendar-month {
-    ${gridGap};
-
-    & .is-prev_month, .is-next_month {
-      color: ${colors.gray_ligth};
+    & .calendar-month {
+      ${gridGap};
     }
 
-    & .is-current_month {
-      color: black;
-      font-weight: bold;
+    & .calendar-days_of_week {
+      ${gridColumns};
+
+      & div {
+        justify-self: center;
+        font-size: ${theme.fontSizes.sm}px;
+        color: ${colors.gray_ligth};
+        padding: 2px 0;
+      }
     }
 
-    & .is-today {
-      padding: 4px;
-      border: 1px solid ${colors.gray_ligth};
-      border-radius: 5px;
-      color: ${colors.blue_ligth};
-    }
-  }
+    & .calendar-week {
+      ${gridColumns};
 
-  & .calendar-days_of_week {
-    ${gridColumns};
+      & button {
+        justify-self: center;
+        width: 60%;
+        padding: 2px 0;
+        border: 1px solid transparent;
+        border-radius: 5px;
+      }
 
-    & div {
-      justify-self: center;
-      font-size: ${theme.fontSizes.sm}px;
-      color: ${colors.gray_ligth};
-    }
-  }
+      & .is-prev_month, .is-next_month {
+        color: ${colors.gray_ligth};
+      }
 
-  & .calendar-week {
-    ${gridColumns};
+      & .is-current_month {
+        color: black;
+        font-weight: bold;
+      }
 
-    & button {
-      justify-self: center;
+      & .is-today {
+        border-color: ${colors.gray_ligth};
+        color: ${colors.blue_ligth};
+      }
+
+      & .is-selected {
+        transition: all 500ms;
+        color: ${colors.white};
+        background-color: ${colors.blue_ligth};
+        border-color: transparent;
+      }
     }
   }
 `;
