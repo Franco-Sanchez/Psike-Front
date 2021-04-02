13435218a6b50e267b30e82a68ac7aac0e51346b
Grecia Delgado <greciadm2596@gmail.com>
2021-04-01 21:59:13 -0500

    distach to fetch appointment almost done

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import CardContainer from "../components/Containers/CardContainer";
 import { Heading3 } from "../components/text/Heading";
 import CardHistory from "../components/UI/CardHistory";
 
 export default function HistoryPage() {
+  const tokenLogin = useSelector((state) => state.session.token);
+  const tokenSignup = useSelector((state) => state.signup.token);
+  const dispatch = useDispatch();
+
+  if (tokenLogin || tokenSignup) {
+    console.log("heyyyy");
+  }
+
   return (
     <>
       <Heading3>Tu historial de citas es: </Heading3>
