47e4d9c7a9e4b4d26eb2b2d53513a4b3628915df
Franco <francorsr98@gmail.com>
2021-04-15 15:27:50 -0500

    Updated description of the calendar

--- src/components/UI/CardSchedule.js
+++ src/components/UI/CardSchedule.js
@@ -138,9 +138,7 @@ export default function CardSchedule({ schedules, appointments, styles }) {
                 ${description}
               `}
             >
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s.
+              Programa tu cita. ¡Te esperamos!
             </Content>
             <ContainerHours>
               <Icon
